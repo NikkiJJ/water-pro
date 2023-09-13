03c1177463d0ace43cc1b1cb6689e456ceb8ddf7
nicola camp <nicola.camp89@yahoo.com>
2023-09-13 10:59:43 +0100

    adding reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,18 @@
 class ReviewsController < ApplicationController
+
+  def new
+    @review = Review.new
+    @bathing_site = bathing_site.find(params[:bathing_site_id])
+    authorize @review
+  end
+
+  def index
+    @reviews = policy_scope(Review).all
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:title, :body, :ratings)
+  end
 end
