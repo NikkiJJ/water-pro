b861081fc65e47f2c9c107e38c9c572c5e024678
Fernando Ribeiro <pontogponto@gmail.com>
2023-09-20 18:52:51 +0200

    seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,17 +5,47 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+#!/usr/bin/env ruby
+
+# Load Rails application environment
+
+require_relative '../config/environment'
+
+require 'nokogiri'
+require 'open-uri'
+require 'httparty'
+require 'json'
+require 'csv'
+
 BathingSite.destroy_all
+User.destroy_all
 
-10.times do
-  bathing_site = BathingSite.new(
-    pollution_level: [1, 2, 3].sample,
-    site_name: ['sea palling', 'brighton', 'blackpool', 'walton', 'southend', 'brightlingsea', 'jaywick', 'clacton', 'margate', 'bromley'].sample,
-    tide: [1, 2, 3].sample,
-    region: ['norfolk', 'london', 'essex', 'blackpool', 'kent', 'cornwall', 'devon'].sample
+user = User.create!(
+  email: "user1@gmail.com",
+  password: "123456",
+  first_name:"John",
+  last_name:"Smith",
+  nickname: "jonny"
+)
+
+csv_path = Rails.root.join('app', 'data', 'site.csv')
+
+CSV.foreach(csv_path, headers: true) do |row|
+  eubwid = row['EUBWID']
+
+   base_url = 'https://environment.data.gov.uk/id/bathing-water/'
+   api_url = "#{base_url}#{eubwid}.json".freeze
+
+  response = HTTParty.get(api_url)
+  data = response.parsed_response
+  quality = data['result']['primaryTopic']
+  water_quality = quality['latestComplianceAssessment']['complianceClassification']['name']['_value']
+  site_name = data['result']['primaryTopic']['name']['_value']
+
+  BathingSite.create!(
+    site_name: site_name,
+    water_quality: water_quality,
+    user: user
   )
-
-  bathing_site.save!
-
-    puts "#{bathing_site.site_name} is made"
+p "Created #{site_name} bathing site!"
 end
