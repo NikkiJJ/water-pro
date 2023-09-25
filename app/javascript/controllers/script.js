594523cf4d1476f42fd21bc917aa58a99c0b1e15
nicola camp <nicola.camp89@yahoo.com>
2023-09-25 12:28:29 +0100

    wip

---
+++ app/javascript/controllers/script.js
@@ -0,0 +1,14 @@
+const text = "This is a typewriter effect example.";
+const textElement = document.getElementById("text");
+
+let index = 0;
+
+function type() {
+  if (index < text.length) {
+    textElement.textContent += text.charAt(index);
+    index++;
+    setTimeout(type, 50); // Adjust the typing speed here (in milliseconds)
+  }
+}
+
+type(); // Start typing when the page loads
