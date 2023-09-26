af47c9157b1ff2034601a63c1a9c321f54ffd939
Lewis Hollis <111375017+lewishollis@users.noreply.github.com>
2023-09-26 13:11:47 +0100

    images for bathing sites seeded

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_23_212934) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_24_113900) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,6 +25,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_23_212934) do
     t.float "latitude"
     t.float "longitude"
     t.string "eubwid"
+    t.string "web_res_image_url"
     t.index ["user_id"], name: "index_bathing_sites_on_user_id"
   end
 
