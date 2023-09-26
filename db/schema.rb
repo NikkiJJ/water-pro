2ad36ce1e9a9cb4078a8294a2f265d5c74d60dc6
nicola camp <nicola.camp89@yahoo.com>
2023-09-26 14:57:40 +0100

    accidentally coded on master branch

--- db/schema.rb
+++ db/schema.rb
@@ -22,7 +22,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_23_212934) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.float "latitude"
+    t.float "latitude" 
     t.float "longitude"
     t.string "eubwid"
     t.index ["user_id"], name: "index_bathing_sites_on_user_id"
