8a2e006ab28138df13a0b4bbdf94f5cedd1c9d6a
Mardgelle Neri <mardgelleneri@gmail.com>
2023-09-26 17:07:05 +0100

    wip

--- app/controllers/report_reviews_controller.rb
+++ app/controllers/report_reviews_controller.rb
@@ -1,12 +1,13 @@
 class ReportReviewsController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :new ]
   def new
-    @report_reviews = ReportReview.new
+    @report_reviews = ReportReview.new[report_reviews_params]
   end
 
   def create
     @report_reviews = ReportReview.new[report_reviews_params]
     @report.review = Review.find(params[:id])
+    authorize @report_reviews
     if @report.save
       redirect_to report_confirmation_page_path(@report_reviews)
     else
