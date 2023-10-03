349554bd09593dcd37d2aa89af8ca3be7f9352ed
Mardgelle Neri <mardgelleneri@gmail.com>
2023-10-03 21:00:43 +0100

    pushing notifications on bathing site show page

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,10 +2,3 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "bootstrap"
-
-import { Application } from "@hotwired/stimulus"
-import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers"
-
-window.Stimulus = Application.start()
-const context = require.context("./controllers", true, /\.js$/)
-Stimulus.load(definitionsFromContext(context))
