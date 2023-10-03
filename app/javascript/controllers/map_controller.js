5b8b313fff135ce689dd159f93cd3b6d3ae94d24
Lewis Hollis <111375017+lewishollis@users.noreply.github.com>
2023-10-03 15:11:36 +0100

    airq info reformatted

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -1,36 +1,42 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="map"
 export default class extends Controller {
-  static values = {apiKey: String, markers: Array}
+  static values = { apiKey: String, markers: Array };
 
   connect() {
-    mapboxgl.accessToken = this.apiKeyValue
+    mapboxgl.accessToken = this.apiKeyValue;
     this.map = new mapboxgl.Map({
-    container: this.element,
-    style: 'mapbox://styles/mapbox/streets-v12',
+      container: this.element,
+      style: "mapbox://styles/mapbox/streets-v12",
     });
-    this.#addMarkersToMap()
-    this.#fitMapToMarkers()
+    this.#addMarkersToMap();
+    this.#fitMapToMarkers();
   }
 
   #addMarkersToMap() {
     this.markersValue.forEach((marker, index) => {
-      const popup = new mapboxgl.Popup().setHTML(
-        `<a href='${marker.popupLink}'>${marker.popupContent}</a>`
-      );
+      const popupContentWithStyles = `
+        <div style='background-color: #fff; border: 1px solid #ccc; border-radius: 10px; padding: 10px; width: 200px;'>
+          <div style="width: 100%; text-align: center;">
+            <a href='${marker.popupLink}' style='text-decoration: none; color: #000000; font-weight: bold;'>${marker.popupContent}</a>
+          </div>
+        </div>
+      `;
+
+      const popup = new mapboxgl.Popup().setHTML(popupContentWithStyles);
       new mapboxgl.Marker()
-        .setLngLat([ marker.lng, marker.lat ])
+        .setLngLat([marker.lng, marker.lat])
         .setPopup(popup)
-        .addTo(this.map)
+        .addTo(this.map);
     });
   }
 
   #fitMapToMarkers() {
-    const bounds = new mapboxgl.LngLatBounds()
+    const bounds = new mapboxgl.LngLatBounds();
     this.markersValue.forEach((marker) => {
-      bounds.extend([ marker.lng, marker.lat ])
-    })
-    this.map.fitBounds(bounds, {})
+      bounds.extend([marker.lng, marker.lat]);
+    });
+    this.map.fitBounds(bounds, {});
   }
 }
